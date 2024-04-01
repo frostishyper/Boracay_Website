f575804cf495496576d894e0a652a6c7739f0bdc
Lee <71824443+frostishyper@users.noreply.github.com>
2024-04-01 19:07:36 +0800

    Ver 1.0
    Preliminary Website Is Complete

---
+++ Beach Website/Scripts/Group.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //Button Links/Redirects
+
+    const homeBtn = document.getElementById('Home-Btn');
+    homeBtn.addEventListener('click', () => {
+    window.location.href = 'index.html';
+    });
+
+    const Lee = document.getElementById('Lee');
+    Lee.addEventListener('click', () => {
+    window.open('https://web.facebook.com/profile.php?id=61550485739728', '_blank');
+    });
+
+    const Precious = document.getElementById('Precious');
+    Precious.addEventListener('click', () => {
+    window.open('https://web.facebook.com/profile.php?id=100089468287804', '_blank');
+    });
+
+    const Carmina = document.getElementById('Carmina');
+    Carmina.addEventListener('click', () => {
+    window.open('https://web.facebook.com/carminaisabel.eugenio', '_blank');
+    });
+
+    const Jhunella = document.getElementById('Jhunella');
+    Jhunella.addEventListener('click', () => {
+    window.open('https://web.facebook.com/jhunella.tayaban.ella', '_blank');
+    });
+
+    const Kathlyn = document.getElementById('Kathlyn');
+    Kathlyn.addEventListener('click', () => {
+    window.open('https://web.facebook.com/kathlyn.abendan.5', '_blank');
+    });
+
+    const Kurt = document.getElementById('Kurt');
+    Kurt.addEventListener('click', () => {
+    window.open('https://web.facebook.com/kurtandrei.santos.33', '_blank');
+    });
+
+    const Dhaniel = document.getElementById('Dhaniel');
+    Dhaniel.addEventListener('click', () => {
+    window.open('https://web.facebook.com/jancprz', '_blank');
+    });
+
+});
